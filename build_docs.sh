3072af4abc9cfd623562543ecba7fab250fb0903
Mark Goodwin <m0good04@gmail.com>
2023-11-21 13:56:22 -0500

    Moved index.html

--- build_docs.sh
+++ build_docs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rustdoc src/lib.rs --crate-name indexable_str
-echo "<meta http-equiv=\"refresh\" content=\"0; url=indexable_str/index.html\">" > doc/index.html
+echo "<meta http-equiv=\"refresh\" content=\"0; url=doc/indexable_str/index.html\">" > ./index.html
